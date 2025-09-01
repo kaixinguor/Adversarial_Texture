685c37ae316ad13371a8ec3b582cf4606ce9f2cd
kaixinguor <linr.w@163.com>
2025-09-01 12:49:14 +0800

    add bash command for testing

---
+++ run.sh
@@ -0,0 +1,5 @@
+# RCA
+python evaluation_texture.py --method RCA --load_path training_results/yolov2_RCA_result/patch2000.npy --suffix yolov2_RCA
+
+# TCA
+python evaluation_texture.py --method TCA --load_path training_results/yolov2_TCA_result/patch2000.npy
